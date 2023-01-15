501a87a5fbc21b543498e23ab399769b9ee37c3d
Roddy Munro <roddymunro@icloud.com>
2023-01-15 11:32:32 -0400

    Initial Commit

---
+++ ModalWrapper/ModalWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalWrapperApp.swift
+//  ModalWrapper
+//
+//  Created by Roddy Munro on 2023-01-15.
+//
+
+import SwiftUI
+
+@main
+struct ModalWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
